d86327f14b6f5f2dc73db96d513d1ad8e2d1721d
jklingsberg <jklingsber@brynmawr.edu>
2015-03-03 20:52:37 -0500

    update activity now has content

--- src/com/example/valleyzapotectalkingdictionary/UpdateActivity.java
+++ src/com/example/valleyzapotectalkingdictionary/UpdateActivity.java
@@ -1,5 +1,6 @@
 package com.example.valleyzapotectalkingdictionary;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +14,9 @@ public class UpdateActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_update);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setTitle("Update");
 	}
 
 	@Override
