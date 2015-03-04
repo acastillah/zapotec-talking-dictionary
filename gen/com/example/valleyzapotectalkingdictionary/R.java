bdaa3f6fa60c487a685d8e51523e82151b48fb77
jklingsberg <jklingsber@brynmawr.edu>
2015-03-03 20:56:20 -0500

    back button fixed for update activity and about activity

--- gen/com/example/valleyzapotectalkingdictionary/R.java
+++ gen/com/example/valleyzapotectalkingdictionary/R.java
@@ -1636,8 +1636,8 @@ containing a value of this type.
         public static final int listener_icon=0x7f020034;
     }
     public static final class id {
-        public static final int FirstWord=0x7f090056;
-        public static final int SecondWord=0x7f090057;
+        public static final int FirstWord=0x7f090054;
+        public static final int SecondWord=0x7f090055;
         public static final int about=0x7f090047;
         public static final int action_bar=0x7f090031;
         public static final int action_bar_activity_content=0x7f090003;
@@ -1660,20 +1660,18 @@ containing a value of this type.
         public static final int always=0x7f090014;
         public static final int application=0x7f090045;
         public static final int beginning=0x7f09001b;
-        public static final int checkBox1=0x7f09004b;
-        public static final int checkBox2=0x7f09004e;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f09002f;
         public static final int default_activity_button=0x7f090024;
         public static final int dialog=0x7f090019;
         public static final int disableHome=0x7f09000d;
-        public static final int downloadAudioCheckbox=0x7f09004a;
-        public static final int downloadPicturesCheckbox=0x7f09004c;
+        public static final int downloadAudioCheckbox=0x7f09004b;
+        public static final int downloadPicturesCheckbox=0x7f09004a;
         public static final int dropdown=0x7f09001a;
         public static final int edit_query=0x7f090033;
         public static final int end=0x7f09001c;
-        public static final int englishInterface=0x7f09005a;
+        public static final int englishInterface=0x7f090058;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
         public static final int home=0x7f090000;
@@ -1681,8 +1679,8 @@ containing a value of this type.
         public static final int icon=0x7f090026;
         public static final int ifRoom=0x7f090016;
         public static final int image=0x7f090023;
-        public static final int imageButton1=0x7f090051;
-        public static final int imageButton2=0x7f090053;
+        public static final int imageButton1=0x7f09004f;
+        public static final int imageButton2=0x7f090051;
         public static final int listMode=0x7f09000a;
         public static final int list_item=0x7f090025;
         public static final int llFragmentContainer=0x7f090040;
@@ -1693,8 +1691,8 @@ containing a value of this type.
         public static final int progress_circular=0x7f090007;
         public static final int progress_horizontal=0x7f090008;
         public static final int radio=0x7f09002b;
-        public static final int searchBox=0x7f090054;
-        public static final int searchButton=0x7f090055;
+        public static final int searchBox=0x7f090052;
+        public static final int searchButton=0x7f090053;
         public static final int search_badge=0x7f090035;
         public static final int search_bar=0x7f090034;
         public static final int search_button=0x7f090036;
@@ -1710,25 +1708,25 @@ containing a value of this type.
         public static final int showCustom=0x7f090010;
         public static final int showHome=0x7f090011;
         public static final int showTitle=0x7f090012;
-        public static final int spanishInterface=0x7f09005b;
+        public static final int spanishInterface=0x7f090059;
         public static final int split_action_bar=0x7f090004;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
         public static final int textView1=0x7f09003f;
-        public static final int textView3=0x7f09004f;
-        public static final int textView4=0x7f090050;
-        public static final int textView5=0x7f090052;
+        public static final int textView3=0x7f09004d;
+        public static final int textView4=0x7f09004e;
+        public static final int textView5=0x7f090050;
         public static final int title=0x7f090027;
         public static final int up=0x7f090001;
         public static final int update=0x7f090044;
-        public static final int updateButton=0x7f09004d;
+        public static final int updateButton=0x7f09004c;
         public static final int updateText=0x7f090049;
         public static final int useLogo=0x7f090013;
         public static final int withText=0x7f090018;
-        public static final int wordOfTheDay=0x7f090058;
-        public static final int wordOfTheDay_word=0x7f090059;
+        public static final int wordOfTheDay=0x7f090056;
+        public static final int wordOfTheDay_word=0x7f090057;
         public static final int wrap_content=0x7f090009;
-        public static final int zapotecInterface=0x7f09005c;
+        public static final int zapotecInterface=0x7f09005a;
     }
     public static final class integer {
         /**  The maximum number of action buttons that should be permitted within
