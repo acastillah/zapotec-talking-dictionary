8ae6d96e2f2f981177c67e47f112c54ddb72e3ed
acastillah <adri0396@hotmail.com>
2015-03-06 13:36:00 -0500

    Final version

--- src/com/example/valleyzapotectalkingdictionary/WordDefinitionActivity.java
+++ src/com/example/valleyzapotectalkingdictionary/WordDefinitionActivity.java
@@ -4,7 +4,6 @@ import com.example.valleyzapotectalkingdictionary.MainActivity.LanguageInterface
 
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
