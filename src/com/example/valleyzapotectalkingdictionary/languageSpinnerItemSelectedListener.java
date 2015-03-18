e6d4f4db2bcd23acce097bff6311d72b4bfd3737
Juliette Klingsberg <julietterklingsberg@gmail.com>
2015-03-17 23:01:47 -0400

    NOTE: DO NOT USE THIS BRANCH, THIS IS JUST A DUMB WAY FOR ME TO BACK UP SOME CODE

---
+++ src/com/example/valleyzapotectalkingdictionary/languageSpinnerItemSelectedListener.java
@@ -0,0 +1,25 @@
+package com.example.valleyzapotectalkingdictionary;
+
+import android.graphics.Color;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.TextView;
+
+public class languageSpinnerItemSelectedListener implements
+		OnItemSelectedListener {
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+		// TODO Auto-generated method stub
+		((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
+
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
