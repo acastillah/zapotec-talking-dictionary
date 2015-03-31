9dc27952711f68dde9e1e99a65bbb34092f9fd01
Juliette Klingsberg <julietterklingsberg@gmail.com>
2015-03-31 14:41:59 -0400

    adding untracked files

---
+++ src/com/example/valleyzapotectalkingdictionary/Preferences.java
@@ -0,0 +1,10 @@
+package com.example.valleyzapotectalkingdictionary;
+
+public class Preferences {
+	public static final String APP_SETTINGS = "APP_SETTINGS";
+	
+	public static final String DOWNLOAD_AUDIO = "DOWNLOAD_AUDIO";
+	public static final String DOWNLOAD_PHOTOS = "DOWNLOAD_PHOTOS";
+	
+	public static final String IS_LINGUIST = "IS_LINGUIST";
+}
