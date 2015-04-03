6ab930c5e7ad168d4fdd639ca265d9767041ac4d
Juliette Klingsberg <julietterklingsberg@gmail.com>
2015-04-03 04:51:36 -0400

    asdfj

--- src/com/example/valleyzapotectalkingdictionary/ImageCaptureFragment.java
+++ src/com/example/valleyzapotectalkingdictionary/ImageCaptureFragment.java
@@ -9,6 +9,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -28,10 +29,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Gallery;
+import android.widget.Gallery.LayoutParams;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -192,6 +197,16 @@ public class ImageCaptureFragment extends Fragment {
 //                    ViewGroup.LayoutParams.WRAP_CONTENT,
 //                    ViewGroup.LayoutParams.WRAP_CONTENT,
 //                    0));
+        
+        
+        Gallery gallery = new Gallery(activity);
+        
+        gallery.setAdapter(new ImageAdapter(activity));
+        
+        Gallery.LayoutParams galleryLayoutParams = (LayoutParams) gallery.getLayoutParams();
+        galleryLayoutParams.height = galleryLayoutParams.WRAP_CONTENT;
+        galleryLayoutParams.width = galleryLayoutParams.FILL_PARENT;
+        gallery.setLayoutParams(galleryLayoutParams);
         
         dispatchTakePictureIntent();
         
@@ -470,6 +485,43 @@ public class ImageCaptureFragment extends Fragment {
 		public FileNameEditText(Context context) {
 			super(context);
 			addTextChangedListener(watcher);
+		}
+		
+	}
+	
+	public class ImageAdapter extends BaseAdapter {
+
+		private Context context;
+		
+		public ImageAdapter(Context c) {
+			context = c;
+//			TypedArray a = obtainStyledAttributes(R.styleable.MyGallery);
+//			itemBackground = a.getResourceId(R.styleable.MyGallery_android_galleryItemBackground, 0);
+//			
+		}
+		
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			// TODO Auto-generated method stub
+			return null;
 		}
 		
 	}
