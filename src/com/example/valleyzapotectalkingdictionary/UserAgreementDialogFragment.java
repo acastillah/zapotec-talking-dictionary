69a6f419eefc370f92a968070619694669b42810
Juliette Klingsberg <julietterklingsberg@gmail.com>
2015-05-08 19:12:32 -0400

    support for Spanish in user agreement

--- src/com/example/valleyzapotectalkingdictionary/UserAgreementDialogFragment.java
+++ src/com/example/valleyzapotectalkingdictionary/UserAgreementDialogFragment.java
@@ -9,6 +9,9 @@ import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class UserAgreementDialogFragment extends DialogFragment {
     @Override
@@ -16,10 +19,22 @@ public class UserAgreementDialogFragment extends DialogFragment {
         // Use the Builder class for convenient dialog construction
     	View dialogView = getActivity().getLayoutInflater().inflate(R.layout.fragment_user_agreement_dialog, null);
     	
+    	Button englishButton = (Button) dialogView.findViewById(R.id.userAgreement_English_Button);
+    	Button spanishButton = (Button) dialogView.findViewById(R.id.userAgreement_Spanish_Button);
+    	TextView englishText = (TextView) dialogView.findViewById(R.id.userAgreement_English);
+    	TextView spanishText = (TextView) dialogView.findViewById(R.id.userAgreement_Spanish);
+    	
+		englishButton.setOnClickListener(new UserAgreementLanguageButtonOnClickListener(englishText, spanishText, englishButton, spanishButton));
+    	spanishButton.setOnClickListener(new UserAgreementLanguageButtonOnClickListener(spanishText, englishText, spanishButton, englishButton));
+    	
+    	// show English by default
+    	englishButton.setEnabled(false);
+    	spanishText.setVisibility(View.GONE);
+    	
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Terms of Use");
+        builder.setTitle("Terms of Use/\nCondiciones de Uso");
         builder.setView(dialogView);
-        builder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("Accept/\nAccept", new DialogInterface.OnClickListener() {
                    public void onClick(DialogInterface dialog, int id) {
 								SharedPreferences preferences = getActivity()
 										.getSharedPreferences(Preferences.APP_SETTINGS,
@@ -29,13 +44,38 @@ public class UserAgreementDialogFragment extends DialogFragment {
 								editor.commit();
                    }
                })
-               .setNegativeButton("Reject", new DialogInterface.OnClickListener() {
+               .setNegativeButton("Reject/\nReject", new DialogInterface.OnClickListener() {
                    public void onClick(DialogInterface dialog, int id) {
                 	   // exit the application
                 	   getActivity().finish();
                    }
                });
+        
         // Create the AlertDialog object and return it
         return builder.create();
+    }
+    
+    public class UserAgreementLanguageButtonOnClickListener implements OnClickListener {
+
+    	TextView show = null;
+    	TextView hide = null;
+    	Button enable = null;
+    	Button disable = null;
+    	
+    	public UserAgreementLanguageButtonOnClickListener(TextView show, TextView hide, Button disable, Button enable) {
+    		this.show = show;
+    		this.hide = hide;
+    		this.disable = disable;
+    		this.enable = enable;
+    	}
+    	
+		@Override
+		public void onClick(View arg0) {
+			show.setVisibility(View.VISIBLE);
+			hide.setVisibility(View.GONE);
+			enable.setEnabled(true);
+			disable.setEnabled(false);
+		}
+    	
     }
 }
