4a7eccfc02f8677e97444a3ac9ee4f9778b0fe28
Juliette Klingsberg <julietterklingsberg@gmail.com>
2015-05-10 02:32:22 -0400

    added deleting files option to settings

--- src/com/example/valleyzapotectalkingdictionary/SettingsFragment.java
+++ src/com/example/valleyzapotectalkingdictionary/SettingsFragment.java
@@ -1,11 +1,22 @@
 package com.example.valleyzapotectalkingdictionary;
 
+import com.example.valleyzapotectalkingdictionary.MainActivity.DeleteAllFilesInDirectoryRunnable;
+import com.example.valleyzapotectalkingdictionary.UploadActivity.DeleteRunnable;
+import com.example.valleyzapotectalkingdictionary.UploadActivity.UploadCompleteDialogFragment;
+import com.example.valleyzapotectalkingdictionary.UploadActivity.UploadHandlerMessageObject;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -14,6 +25,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SettingsFragment extends Fragment {
@@ -21,6 +33,7 @@ public class SettingsFragment extends Fragment {
 	EditText usernameEditText = null;
 	Button saveButton = null;
 	Button uploadButton = null;
+	Button deleteButton = null;
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +46,7 @@ public class SettingsFragment extends Fragment {
         usernameEditText = (EditText) view.findViewById(R.id.username_editText);
         saveButton = (Button) view.findViewById(R.id.settings_submitButton);
         uploadButton = (Button) view.findViewById(R.id.upload_button);
+        deleteButton = (Button) view.findViewById(R.id.delete_button);
         
         // fill in username if one already exists
         SharedPreferences preferences = getActivity().getSharedPreferences(Preferences.APP_SETTINGS, Activity.MODE_PRIVATE);
@@ -43,6 +57,7 @@ public class SettingsFragment extends Fragment {
 		usernameEditText.addTextChangedListener(new usernameEditTextWatcher());
 		saveButton.setOnClickListener(new saveButtonOnClickListener());
 		uploadButton.setOnClickListener(new uploadButtonOnClickListener());
+		deleteButton.setOnClickListener(new deleteButtonOnClickListener(getActivity()));
         
 		return view;
     }
@@ -97,5 +112,49 @@ public class SettingsFragment extends Fragment {
 		    startActivity(intent);
 		}
 		
+	}
+	
+	public class deleteButtonOnClickListener implements OnClickListener {
+
+		FragmentActivity activity = null;
+		
+		public deleteButtonOnClickListener(FragmentActivity activity) {
+			this.activity = activity;
+		}
+		
+		@Override
+		public void onClick(View arg0) {
+			new deleteFilesDialogFragment().show(activity.getSupportFragmentManager(), "Dialog");
+		}
+		
+	}
+	
+	public class deleteFilesDialogFragment extends DialogFragment {
+				
+	    @Override
+	    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+	    	AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+	        builder.setTitle("Are you sure you want to delete all photos and audio files?");
+
+	    	
+	    		builder.setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
+	                   public void onClick(DialogInterface dialog, int id) {
+
+	                	   new Thread(((MainActivity)getActivity()).new DeleteAllFilesInDirectoryRunnable(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Zapotec Talking Dictionary/photos")).start();
+	                	   new Thread(((MainActivity)getActivity()).new DeleteAllFilesInDirectoryRunnable(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Zapotec Talking Dictionary/audio")).start();
+	                	   Toast.makeText(getActivity(), "Files deleted", Toast.LENGTH_SHORT).show();
+	                   }
+	               });
+	        
+
+		        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+		                   public void onClick(DialogInterface dialog, int id) {
+		                	   
+		                   }
+		               });
+	    	
+	        return builder.create();
+	    }
 	}
 }
